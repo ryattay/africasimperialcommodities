27d640bad23aea89ccee419f20b585ed1979a83d
r <39985574+cartyrya@users.noreply.github.com>
2019-02-15 11:05:39 -0500

    Edit data visualization
    Edit tooltip for bar chart test a

--- js/barchart_skins_senegal.js
+++ js/barchart_skins_senegal.js
@@ -24,7 +24,7 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10,0])
   .html(function(d) {
-    return "Frequency: " + d.vx;
+    return d.vx + " metric tons";
   });
 
 // add the SVG element
