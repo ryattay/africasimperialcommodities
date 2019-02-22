12b8a5e23b9bce3893aa07bf3eaaa92ca37a3986
r <39985574+cartyrya@users.noreply.github.com>
2019-02-22 12:16:21 -0500

    Edit data visualization

--- js/scatterplot_skins.js
+++ js/scatterplot_skins.js
@@ -1,22 +1,22 @@
 var margin1 = {top: 20, right: 20, bottom: 40, left: 40},
-    width1 = 760 - margin.left - margin.right,
-    height1 = 500 - margin.top - margin.bottom;
+    width1 = 760 - margin1.left - margin1.right,
+    height1 = 500 - margin1.top - margin1.bottom;
 
 var xValue1 = function(d) { return d.year;},
-    xScale1 = d3.scale.linear().range([0, width]),
-    xMap1 = function(d) { return xScale(xValue(d));},
-    xAxis1 = d3.svg.axis().scale(xScale).orient("bottom");
+    xScale1 = d3.scale.linear().range([0, width1]),
+    xMap1 = function(d) { return xScale1(xValue1(d));},
+    xAxis1 = d3.svg.axis().scale(xScale1).orient("bottom");
 
 var yValue1 = function(d) { return d.vx;},
-    yScale1 = d3.scale.linear().range([height, 0]),
-    yMap1 = function(d) { return yScale(yValue(d));},
-    yAxis1 = d3.svg.axis().scale(yScale).orient("left");
+    yScale1 = d3.scale.linear().range([height1, 0]),
+    yMap1 = function(d) { return yScale1(yValue1(d));},
+    yAxis1 = d3.svg.axis().scale(yScale1).orient("left");
 
 var svg1 = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
 
 var tooltip1 = d3.select("body").append("div")
     .attr("class", "tooltip")
@@ -24,23 +24,23 @@ var tooltip1 = d3.select("body").append("div")
 
 d3.json("./data/animalSkins.json", function(error, data) {
 
-  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+  xScale1.domain([d3.min(data, xValue1)-1, d3.max(data, xValue1)+1]);
+  yScale1.domain([d3.min(data, yValue1)-1, d3.max(data, yValue1)+1]);
 
   svg1.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + height1 + ")")
+      .call(xAxis1)
     .append("text")
       .attr("class", "label")
-      .attr("x", width)
+      .attr("x", width1)
       .attr("y", -6)
       .style("text-anchor", "end")
       .text("Year");
 
   svg1.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(yAxis1)
     .append("text")
       .attr("class", "label")
       .attr("transform", "rotate(-90)")
@@ -54,18 +54,18 @@ d3.json("./data/animalSkins.json", function(error, data) {
     .enter().append("circle")
       .attr("class", "dot")
       .attr("r", 3.5)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
+      .attr("cx", xMap1)
+      .attr("cy", yMap1)
       .on("mouseover", function(d) {
-        tooltip.transition()
+        tooltip1.transition()
           .duration(200)
           .style("opacity", .9);
-        tooltip.html("Place of export: " + d["cntr_desc"] + "<br/> Product: " + d["prdt_desc"] + "<br/> Year: " + xValue(d) + "<br/> Quantity: " + yValue(d) + " metric tons")
+        tooltip1.html("Place of export: " + d["cntr_desc"] + "<br/> Product: " + d["prdt_desc"] + "<br/> Year: " + xValue1(d) + "<br/> Quantity: " + yValue1(d) + " metric tons")
           .style("left", (d3.event.pageX + 5) + "px")
           .style("top", (d3.event.pageY - 28) + "px");
       })
       .on("mouseout", function(d) {
-        tooltip.transition()
+        tooltip1.transition()
           .duration(500)
           .style("opacity", 0);
       });
