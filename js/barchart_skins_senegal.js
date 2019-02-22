02e81b24f74fb33488290f32e868b17a3bf64134
r <39985574+cartyrya@users.noreply.github.com>
2019-02-22 14:31:02 -0500

    Edit data visualization

--- js/barchart_skins_senegal.js
+++ js/barchart_skins_senegal.js
@@ -1,26 +1,26 @@
 // Canvas dimensions
-var margin = {top: 20, right: 20, bottom: 40, left: 20},
-    width = 760 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin2 = {top: 20, right: 20, bottom: 40, left: 40},
+    width2 = 760 - margin2.left - margin2.right,
+    height2 = 500 - margin2.top - margin2.bottom;
 
 // Set the ranges
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .05);
+var x2 = d3.scale.ordinal()
+    .rangeRoundBands([0, width2], .05);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var y2 = d3.scale.linear()
+    .range([height2, 0]);
 
 // Define the axis
-var xAxis = d3.svg.axis()
-  .scale(x)
+var xAxis2 = d3.svg.axis()
+  .scale(x2)
   .orient("bottom");
 
-var yAxis = d3.svg.axis()
-  .scale(y)
+var yAxis2 = d3.svg.axis()
+  .scale(y2)
   .orient("left")
   .ticks(10);
 
-var tip = d3.tip()
+var tip2 = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10,0])
   .html(function(d) {
@@ -28,27 +28,27 @@ var tip = d3.tip()
   });
 
 // add the SVG element
-var svg2 = d3.select("#svg2")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+var svg2 = d3.select(".chart")
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + margin2.left + "," + margin2.top + ")");
 
-svg2.call(tip);
+svg2.call(tip2);
 
 // load the data
 d3.json("./data/animalSkins_senegal.json", function(error, data) {
 
   // scale the range of the data
-  x.domain(data.map(function(d) { return d.year; }));
-  y.domain([0, d3.max(data, function(d) { return d.vx; })]);
+  x2.domain(data.map(function(d) { return d.year; }));
+  y2.domain([0, d3.max(data, function(d) { return d.vx; })]);
 
   // add axis
   svg2.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis2)
     .selectAll("text")
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
@@ -57,7 +57,7 @@ d3.json("./data/animalSkins_senegal.json", function(error, data) {
 
   svg2.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(yAxis2)
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 5)
@@ -82,7 +82,7 @@ d3.json("./data/animalSkins_senegal.json", function(error, data) {
       .attr("x", function(d) { return x(d.year); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.vx); })
-      .attr("height", function(d) { return height - y(d.vx); })
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide)
+      .attr("height", function(d) { return height2 - y(d.vx); })
+      .on("mouseover", tip2.show)
+      .on("mouseout", tip2.hide)
 });
