7026e5b4f8cf7fbc857897c6d0d5f1a1dec05a71
r <39985574+cartyrya@users.noreply.github.com>
2019-02-22 15:36:31 -0500

    Edit data visualization

--- js/barchart_skins_nigeria.js
+++ js/barchart_skins_nigeria.js
@@ -1,26 +1,26 @@
 // Canvas dimensions
-var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 760 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin3 = {top: 20, right: 20, bottom: 40, left: 40},
+    width3 = 760 - margin3.left - margin3.right,
+    height3 = 500 - margin3.top - margin3.bottom;
 
 // Set the ranges
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .05);
+var x3 = d3.scale.ordinal()
+    .rangeRoundBands([0, width3], .05);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var y3 = d3.scale.linear()
+    .range([height3, 0]);
 
 // Define the axis
-var xAxis = d3.svg.axis()
-  .scale(x)
+var xAxis3 = d3.svg.axis()
+  .scale(x3)
   .orient("bottom");
 
-var yAxis = d3.svg.axis()
-  .scale(y)
+var yAxis3 = d3.svg.axis()
+  .scale(y3)
   .orient("left")
   .ticks(10);
 
-var tip = d3.tip()
+var tip3 = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10,0])
   .html(function(d) {
@@ -29,26 +29,26 @@ var tip = d3.tip()
 
 // add the SVG element
 var svg3 = d3.select("#svg3")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width3 + margin3.left + margin3.right)
+    .attr("height", height3 + margin3.top + margin3.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + margin3.left + "," + margin3.top + ")");
 
-scg3.call(tip);
+svg3.call(tip3);
 
 // load the data
 d3.json("./data/animalSkins_nigeria.json", function(error, data) {
 
   // scale the range of the data
-  x.domain(data.map(function(d) { return d.year; }));
-  y.domain([0, d3.max(data, function(d) { return d.vx; })]);
+  x3.domain(data.map(function(d) { return d.year; }));
+  y3.domain([0, d3.max(data, function(d) { return d.vx; })]);
 
   // add axis
   svg3.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + height3 + ")")
+      .call(xAxis3)
     .selectAll("text")
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
@@ -57,7 +57,7 @@ d3.json("./data/animalSkins_nigeria.json", function(error, data) {
 
   svg3.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(yAxis3)
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 5)
@@ -65,24 +65,15 @@ d3.json("./data/animalSkins_nigeria.json", function(error, data) {
       .style("text-anchor", "end")
       .text("Metric tons");
 
-  // chart.append("g")
-  //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  //     .selectAll(".textlabel")
-  //     .data(data)
-  //   .enter().append("text")
-  //     .style("fill", "black")
-  //     .attr("class", "textlabel")
-  //     .attr("y", function(d) { return y(d.vx) - 3; });
-
   // Add bar chart
   svg3.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.year); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.vx); })
-      .attr("height", function(d) { return height - y(d.vx); })
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide)
+      .attr("x", function(d) { return x3(d.year); })
+      .attr("width", x3.rangeBand())
+      .attr("y", function(d) { return y3(d.vx); })
+      .attr("height", function(d) { return height3 - y3(d.vx); })
+      .on("mouseover", tip3.show)
+      .on("mouseout", tip3.hide)
 });
