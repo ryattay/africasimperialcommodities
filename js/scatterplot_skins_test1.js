75de175fe3705ce4586723d624c05624734918d8
r <39985574+cartyrya@users.noreply.github.com>
2019-03-29 12:15:23 -0400

    Edit data visualization test b

--- js/scatterplot_skins_test1.js
+++ js/scatterplot_skins_test1.js
@@ -20,18 +20,18 @@ d3.json("./data/animalSkins.json", function(error, data) {
 
   var xAxis = chart.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.chart.axis(x));
 
   var y = d3.scale.linear()
     .domain([0, 2250])
     .range([height, 0]);
 
   var yAxis = chart.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.chart.axis(y));
 
   var clip = chart.append("defs").append("chart:clipPath")
     .attr("id", "clip")
-    .append("SVG:rect")
+    .append("chart:rect")
     .attr("width", width)
     .attr("height", height)
     .attr("x", 0)
