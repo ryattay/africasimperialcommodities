c050a8e3a16cafead4162784d58a5390a5e35d1d
r <39985574+cartyrya@users.noreply.github.com>
2019-04-05 16:30:36 -0400

    Revert "Edit data visualiation test d"
    This reverts commit 5259062a6f51546a6e60e38ce8ce386574a654ec.

--- js/scatterplot_skins_test1.js
+++ js/scatterplot_skins_test1.js
@@ -70,7 +70,7 @@ d3.json("./data/animalSkins.json", function(error, data) {
         .attr("width", width)
         .attr("height", height)
         .style("fill", "none")
-        .style("pointer-events", "none")
+        .style("pointer-events", "all")
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
         .call(zoom);
 
