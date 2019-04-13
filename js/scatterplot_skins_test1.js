4cfd721f7eecaea3c7e660195750fcd12953e2ac
r <39985574+cartyrya@users.noreply.github.com>
2019-04-12 21:00:54 -0400

    Revert "Edit data visualization test d"
    This reverts commit dc3164baa3c4a2ccee4cd6c723e87d6e12694f72.

--- js/scatterplot_skins_test1.js
+++ js/scatterplot_skins_test1.js
@@ -20,7 +20,6 @@ d3.json("./data/animalSkins.json", function(error, data) {
 
   var xAxis = chart.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .tickFormat(d3.format("d"))
     .call(d3.axisBottom(x));
 
   var y = d3.scaleLinear()
@@ -57,9 +56,8 @@ d3.json("./data/animalSkins.json", function(error, data) {
   scatter.selectAll(".dot")
       .data(data)
     .enter().append("circle")
-      .style("fill","steelblue")
       .attr("class", "dot")
-      .attr("r", 5)
+      .attr("r", 4)
       .attr("cx", function (d) { return x(d.year); })
       .attr("cy", function (d) {return y(d.vx); })
       .on("mouseover", function(d) {
